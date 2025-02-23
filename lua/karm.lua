e73014f85b2e3a7fafe621f0d4c38c8193434462
ujsquared <ujsquared@gmail.com>
2025-02-24 00:38:34 +0530

    feat: add keymappable toggle function

--- lua/karm.lua
+++ lua/karm.lua
@@ -1,13 +1,27 @@
 local augroup = vim.api.nvim_create_augroup("karm", { clear = true })
 
-local function main()
-    local new_window = vim.api.nvim_open_win(0, false, {
+Todo_exist_global = false
+Todo_buffer_global = nil
+local function toggle_todo()
+    if Todo_exist_global then
+        vim.api.nvim_win_close(Todo_buffer_global, true)
+        Todo_exist_global = false
+        return
+    end
+    Todo_buffer_global = vim.api.nvim_open_win(0, false, {
         split='right',
-        width=70,
+        width=40,
         win= 0
     })
-    vim.api.nvim_tabpage_set_win(0,new_window)
+    vim.api.nvim_tabpage_set_win(0,Todo_buffer_global)
     vim.cmd('e ~/todo.md')
+    Todo_exist_global = true
+end
+
+local function main()
+    local curr_window = vim.api.nvim_get_current_win()
+    toggle_todo()
+    vim.api.nvim_tabpage_set_win(0,curr_window)
 end
 
 local function setup()
@@ -18,4 +32,4 @@ local function setup()
     })
 end
 
-return { setup = setup }
+return { setup = setup, toggle_todo = toggle_todo}
